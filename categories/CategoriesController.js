1caa099dc16b95e181cd8a599b09902ee13614ec
Ana <ana.souza0087@gmail.com>
2023-02-07 21:28:26 -0300

    (feat): create models

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const slugify = require('slugify')
 const router = express.Router()
+const Category = require('./Category')
 
 router.get("/categories", (req, res) => {
     res.send('Categories Route')
 })
 
 router.get("/admin/categories/new", (req, res) => {
-    res.send("Create a new category")
+    res.render("admin/categories/new")
+})
+
+router.post("/categories/save", (req, res) => {
+    const title = req.body.title
+    if (title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/')
+        })
+    } else {
+        res.redirect('/admin/categories/new')
+    }
 })
 
 module.exports = router
