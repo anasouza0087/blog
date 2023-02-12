1d5397ae6f344bf22a828c95a3b6bf17d1c79014
Ana <ana.souza0087@gmail.com>
2023-02-12 20:45:33 -0300

    (feat): finish articles functionalities

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,52 @@
 const express = require('express')
 const router = express.Router()
+const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
-router.get("/articles", (req, res) => {
-    res.send('Articles Route')
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render("admin/articles/index", { articles: articles })
+    })
 })
 
 router.get("/admin/articles/new", (req, res) => {
-    res.send("Create a brand new article")
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", { categories: categories })
+    })
+})
+
+router.post("/articles/save", (req, res) => {
+    const title = req.body.title
+    const body = req.body.body
+    const category = req.body.categories
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        category: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+})
+
+router.post("/articles/delete", (req, res) => {
+    const id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => res.redirect("/admin/articles"))
+        } else {
+            res.redirect("/admin/articles")
+        }
+    } else {
+        res.redirect("/admin/articles")
+    }
 })
 
 module.exports = router
