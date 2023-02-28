a128445038a7c9c19d43af2e92490221d07e95db
Ana <ana.souza0087@gmail.com>
2023-02-27 21:55:44 -0300

    (feat): create crypt password route

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,13 +1,47 @@
 const express = require("express")
 const router = express.Router()
 const User = require("./User")
+const bcrypt = require('bcryptjs')
 
 router.get("/admin/users", (req, res) => {
-    res.send("Listagem de usuários")
+    User.findAll()
+        .then(users => {
+            res.render("admin/users/index", { users: users })
+        })
 })
 
 router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create")
+})
+
+router.post("/users/create", (req, res) => {
+    //title == email
+    const title = req.body.title
+    var password = req.body.password
+
+    User.findOne({
+        where: {
+            title: title
+        }
+    }).then(user => {
+        if (user == undefined) {
+            const salt = bcrypt.genSaltSync(10)
+            const hash = bcrypt.hashSync(password, salt)
+
+            User.create({
+                title: title,
+                password: hash
+            }).then(() => {
+                res.redirect("/")
+            }).catch((err) => {
+                console.log(err)
+            })
+        } else {
+            res.redirect("/admin/users")
+        }
+    })
+
+
 })
 
 module.exports = router
